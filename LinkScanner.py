b19f44f3a9cbbcef421ee3dbaa89c7aaabadacee
SerLink04 <66963108+SerLink04@users.noreply.github.com>
2021-10-01 01:51:54 +0200

    Update LinkScanner.py

--- LinkScanner.py
+++ LinkScanner.py
@@ -10,9 +10,9 @@ import ctypes
 from datetime import datetime
 import re
 
-# Coded by SerLink04 (discord.gg/sweet)
+# Coded by SerLink04 (discord.gg/spain)
 
-name_cmd = "LinkScanner | discord.gg/sweet | Coded by SerLink04"
+name_cmd = "LinkScanner | discord.gg/spain | Coded by SerLink04"
 
 try:
     ctypes.windll.kernel32.SetConsoleTitleW(name_cmd)
